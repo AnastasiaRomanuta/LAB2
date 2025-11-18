ccb88e9608fe0157f2514692a831fb6dec6e7092
Анастасия <varnavskaya.anastasis@gmail.com>
2025-11-18 13:12:20 +0300

    функции для генерации

--- generator.py
+++ generator.py
@@ -2,5 +2,9 @@
 
 import string
 import random
-
+#функции для генерации
+def generate_password(length):
+    characters = string.ascii_letters + string.digits + string.punctuation
+    password = ''.join(random.choice(characters) for _ in range(length))
+    return password
 
